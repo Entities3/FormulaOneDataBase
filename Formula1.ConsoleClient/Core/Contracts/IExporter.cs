2e68170c70ce22236b8bce53edd2ff8cdead7b55
Levent <Levent@FIFTYSHADES>
2017-06-12 10:36:08 +0300

    Added exporter

---
+++ Formula1.ConsoleClient/Core/Contracts/IExporter.cs
@@ -0,0 +1,9 @@
+﻿namespace Formula1.ConsoleClient.Core.Contracts
+{
+    using System.Collections.Generic;
+
+    public interface IExporter
+    {
+        void Export(string path, IDictionary<string, string> list, IEnumerable<string> headers);
+    }
+}
---
+++ Formula1.ConsoleClient/Core/Providers/PdfExporter.cs
@@ -0,0 +1,64 @@
+﻿namespace Formula1.ConsoleClient.Core.Providers
+{
+    using Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using iTextSharp.text;
+    using iTextSharp.text.pdf;
+
+    public class PdfExporter : IExporter
+    {
+        public void Export(string path, IDictionary<string, string> list, IEnumerable<string> headers)
+        {
+            PdfDocument pdfDoc = new PdfDocument();
+            Document ExportDoc = new Document();
+
+            PdfWriter.GetInstance(ExportDoc, new FileStream(path + "/F1PDF_" + DateTime.Now.ToString("s").Replace(":", "") + ".pdf", FileMode.Create));
+            ExportDoc.Open();
+
+
+            PdfPTable table = new PdfPTable(3);
+            table.SetWidths(new float[] { 1, 7, 7 });
+
+
+            PdfPCell firstcell = new PdfPCell(new Phrase());
+            firstcell.Border = Rectangle.BOTTOM_BORDER;
+            table.AddCell(firstcell);
+
+            foreach (var item in headers)
+            {
+                PdfPCell cell = new PdfPCell(new Phrase(item));
+                cell.Border = Rectangle.BOTTOM_BORDER;
+                table.AddCell(cell);
+            }
+
+            table.CompleteRow();
+
+            int i = 0;
+            foreach (var item in list)
+            {
+                i++;
+
+                PdfPCell cell = new PdfPCell(new Phrase(i.ToString() + '.'));
+                cell.Border = Rectangle.NO_BORDER;
+                table.AddCell(cell);
+
+                PdfPCell cell1 = new PdfPCell(new Phrase(item.Key));
+                cell1.Border = Rectangle.LEFT_BORDER;
+                table.AddCell(cell1);
+
+
+                PdfPCell cell2 = new PdfPCell(new Phrase(item.Value));
+                cell2.Border = Rectangle.LEFT_BORDER;
+                table.AddCell(cell2);
+
+                table.CompleteRow();
+            }
+
+            ExportDoc.Add(table);
+            
+            ExportDoc.Close();
+        }
+    }
+}
